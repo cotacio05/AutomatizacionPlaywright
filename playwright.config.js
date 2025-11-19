edbf2823c1b9a69e74d2ab827f48fd90692eec1a
Edward Felipe Leon <edwardfelipeleoncotacio@outlook.com>
2025-11-19 16:12:22 -0500

    Se actualiza tiempos en archivo playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -3,11 +3,11 @@ const { expect } = require("playwright/test");
 module.exports = {
   use: {
     launchOptions: {
-      slowMo: 3000, // ralentiza cada acción 3000ms
+      slowMo: 1000, // ralentiza cada acción 1000ms
     },
     headless: false,  // Usar 'true' para ejecución en modo headless
     screenshot: 'only-on-failure', // Captura de pantalla solo si falla
     video: 'on', // Graba video siempre
   },
-  timeout: 60000 // opcional: extiende el tiempo total del test
+  timeout: 10000 // opcional: extiende el tiempo total del test
 };
