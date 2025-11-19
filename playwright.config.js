f30f6b4f03877322fadab2ac35ed532b5a7e3a6f
Edward Felipe Leon <edwardfelipeleoncotacio@outlook.com>
2025-11-19 14:51:18 -0500

    Se actualiza el archivo playwright.config.js dejando que  permita grabar video siempre

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,6 @@ module.exports = {
   use: {
     headless: false,  // Usar 'true' para ejecución en modo headless
     screenshot: 'only-on-failure', // Captura de pantalla solo si falla
-    video: 'retain-on-failure', // Graba video solo si falla
+    video: 'on', // Graba video siempre
   },
 };
