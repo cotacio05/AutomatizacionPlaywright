e9341387bdbf717492fcf13302db3a6cb5c32904
Edward Felipe Leon <edwardfelipeleoncotacio@outlook.com>
2025-11-19 16:10:58 -0500

    Se realiza actualización de tiempo de ejecución de text en archivo playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,13 @@
+const { expect } = require("playwright/test");
+
 module.exports = {
   use: {
+    launchOptions: {
+      slowMo: 3000, // ralentiza cada acción 3000ms
+    },
     headless: false,  // Usar 'true' para ejecución en modo headless
     screenshot: 'only-on-failure', // Captura de pantalla solo si falla
     video: 'on', // Graba video siempre
   },
+  timeout: 60000 // opcional: extiende el tiempo total del test
 };
